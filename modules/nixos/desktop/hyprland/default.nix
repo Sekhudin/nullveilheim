872ef374307ea3a24d35785ad8b6497e245a8734
sekhudin <sekhudinuap@gmail.com>
2026-07-25 06:50:12 +0700

    chore: remove unused module

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -1,13 +1,26 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
+let
+  isHyprland = config.nixosDesktopModules.use == "hyprland";
+  masterEnable = config.nixosDesktopModules.enable;
+in
 {
-  imports = [ ./core.nix ];
-
   options.nixosDesktopModules.hyprland = {
     settings = lib.mkOption {
       type = lib.types.attrs;
       description = "hyprland settings";
       default = { };
+    };
+  };
+
+  config = lib.mkIf (masterEnable && isHyprland) {
+    programs = {
+      hyprland = {
+        enable = true;
+        withUWSM = true;
+        xwayland.enable = true;
+        systemd.setPath.enable = true;
+      };
     };
   };
 }
