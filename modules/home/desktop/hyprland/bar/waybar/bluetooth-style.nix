35dbd3426966c4f58360102bef264bb649fecbb7
sekhudin <sekhudinuap@gmail.com>
2026-08-07 01:37:39 +0700

    feat: hyprland - add waybar bluetooth

---
+++ modules/home/desktop/hyprland/bar/waybar/bluetooth-style.nix
@@ -0,0 +1,25 @@
+{ font, styles, ... }:
+
+''
+  #bluetooth {
+    color: @fg;
+    background: @bg;
+    font-size: ${toString (font.sizes.bar + 1)}px;
+    min-width: ${toString styles.min_width}px;
+    padding: ${toString styles.padding_y}px ${toString (styles.padding_x - styles.gaps_in)}px;
+    border: ${toString styles.border_size}px solid @border;
+    border-radius: ${toString styles.rounding}px;
+  }
+
+  #bluetooth:hover {
+    border: ${toString styles.border_size}px solid @primary;
+  }
+
+  #bluetooth.on {
+    border: ${toString styles.border_size}px solid @primary;
+  }
+
+  #bluetooth.off {
+    border: ${toString styles.border_size}px solid @border;
+  }
+''
---
+++ modules/home/desktop/hyprland/bar/waybar/bluetooth.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.homeDesktopModules.hyprland;
+  enableWaybar = (cfg.bar.use == "waybar");
+in
+{
+  config = lib.mkIf (cfg.enable && enableWaybar) {
+    programs = {
+      waybar.settings = rec {
+        primary = {
+          "bluetooth" = {
+            format = "{icon} ";
+            format-on = "󰂯";
+            format-off = "󰂲";
+            format-connected = "󰂯";
+            format-disabled = "󰂲";
+            align = 0.5;
+            justify = "center";
+            tooltip = false;
+          };
+        };
+        secondary = primary;
+      };
+    };
+  };
+}
